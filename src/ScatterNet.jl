2e7d11a4b3e7924ef6983a860d0f54afd86cfc26
noso <nas779@usask.ca>
2026-09-02 22:56:37 -0400

    migrating to julia

--- src/ScatterNet.jl
+++ src/ScatterNet.jl
@@ -1,5 +1,4 @@
 """
-ScatterNet — forward model for X-ray/neutron scattering shape reconstruction.
 Submodules: `Cache`, `Interfaces`, `AtomicRadii`, `SphFuncs`, `Molecules`, `FormFactorXrayDB`.
 """
 module ScatterNet
@@ -18,11 +17,10 @@ using .Interfaces: Interfaces, RadiiSource, FormFactorSource
 using .AtomicRadii: AtomicRadii, Ion, tryparse_ion, resolve_one, AtomicRadiiSource
 using .SphFuncs: SphFuncs, sphHarm, sphBess, legendre_sphPlm, SphHarmError, SphBessError
 using .Molecules: Molecules, Molecule, MoleculeError
-using .FormFactorXrayDB: FormFactorXrayDB, FF, FormFactorError,
-                         compute_form_factors, FormFactorSourceXrayDB
+using .FormFactorXrayDB: FormFactorXrayDB, FF, FormFactorError, compute_form_factors, FormFactorSourceXrayDB
 
 export Molecule, MoleculeError, Ion, tryparse_ion, resolve_one, AtomicRadiiSource,
-       sphHarm, sphBess, legendre_sphPlm, SphHarmError, SphBessError,
-       compute_form_factors, FormFactorSourceXrayDB, FF, FormFactorError
+    sphHarm, sphBess, legendre_sphPlm, SphHarmError, SphBessError,
+    compute_form_factors, FormFactorSourceXrayDB, FF, FormFactorError
 
 end # module
